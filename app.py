24960f7359a80106693cce3f8126ca941bb1b870
Mohsan Ali <mohsanali1510@gmail.com>
2026-07-22 07:52:12 -0700

    Configure Flask-Mail SMTP settings

--- app.py
+++ app.py
@@ -23,6 +23,15 @@ db.init_app(app)
 
 migrate.init_app(app, db)
 
+
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_USERNAME"] = "your_email@gmail.com"
+app.config["MAIL_PASSWORD"] = "your_password"
+
+
 mail.init_app(app)
 
 @app.route("/")
